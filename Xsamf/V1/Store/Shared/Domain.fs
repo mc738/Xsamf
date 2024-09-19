fd293d3790da7c422ec7b5cb7bd53e350cd1609a
max <me@mclifford.dev>
2024-09-19 20:46:30 +0100

    Work on store

--- Xsamf/V1/Store/Shared/Domain.fs
+++ Xsamf/V1/Store/Shared/Domain.fs
@@ -1,2 +1,37 @@
-module Xsamf.V1.Store.Shared.Domain
+namespace Xsamf.V1.Store.Shared
 
+open System
+
+module Domain =
+    type NewTenant =
+        { Reference: string
+          Name: string
+          CreatedOn: DateTime option
+          Metadata: Map<string, string>
+          Tags: string list }
+
+    type TenantDetails =
+        { Reference: string
+          Name: string
+          Active: bool
+          CreatedOn: DateTime
+          UpdatedOn: DateTime
+          Metadata: Map<string, string> }
+
+    type TenantOverview = { Reference: string; Name: string }
+
+    [<RequireQualifiedAccess>]
+    type ListTenantsRequest =
+        | Wildcard
+        | WithTag of Tag: string
+        | WithActiveStatus of Status: ListTenantsRequestActiveStatus
+        | All of ListTenantsRequest list
+        | Any of ListTenantsRequest list
+
+
+    and [<RequireQualifiedAccess>] ListTenantsRequestActiveStatus =
+        | Active
+        | NotActive
+        | Any
+
+    ()
--- Xsamf/V1/Store/Shared/IXsamfStore.fs
+++ Xsamf/V1/Store/Shared/IXsamfStore.fs
@@ -1,7 +1,12 @@
 namespace Xsamf.V1.Store.Shared
 
 open FsToolbox.Core.Results
+open Xsamf.V1.Store.Shared.Domain
 
 type IXsamfStore =
     
-    abstract member AddTenant: string -> ActionResult<unit>
+    abstract member AddTenant: NewTenant -> ActionResult<unit>
+    
+    abstract member GetTenant: Reference: string ->  FetchResult<TenantDetails>
+    
+    abstract member ListTenants: Request: ListTenantsRequest -> FetchResult<TenantOverview list>
