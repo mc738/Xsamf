3643935a26f3bf53590e396bcb19e00f7cd44ea0
max <me@mclifford.dev>
2024-09-20 21:42:20 +0100

    Work on Dms

--- Xsamf/V1/Store/Shared/Domain/Dms.fs
+++ Xsamf/V1/Store/Shared/Domain/Dms.fs
@@ -2,14 +2,19 @@ namespace Xsamf.V1.Store.Shared.Domain
 
 module Dms =
 
+    open System
     open Common
 
     type NewDmsRequest =
         { TenantReference: string
           EntityReference: string
           Reference: NewReferenceType
-          CreatedTimestamp: TimestampType }
+          CreatedTimestamp: TimestampType
+          CronPeriod: string          
+          GracePeriod: TimeSpan
+          Tags: string list }
 
     type DmsHeartBeat =
         { TenantReference: string
-          Reference: string }
+          Reference: string
+          Timestamp: TimestampType }
