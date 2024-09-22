49ccfba42e6a7653dd5bb6f89147f9969d5db36d
max <me@mclifford.dev>
2024-09-22 09:45:58 +0100

    Work on Dms

---
+++ Xsamf/V1/Services/Monitoring/Dms.fs
@@ -0,0 +1,18 @@
+namespace Xsamf.V1.Services.Monitoring
+
+open Xsamf.V1.Domain.Monitoring.Dms
+open Xsamf.V1.Store.Shared
+
+
+type DmsService(store: IXsamfStore) =
+    
+    
+    member _.AnonymousCheckIn(checkIn: AnonymousDmsCheckIn) =
+        // First get the watcher
+        
+        
+        // First try and add the check in
+        
+        
+        ()
+
