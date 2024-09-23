5197b9c83f25bd8b347c8a65d9f54548326fd0df
max <me@mclifford.dev>
2024-09-23 09:42:08 +0100

    Work on Common.fs

---
+++ Xsamf/V1/Operations/Monitoring/Dms/Common.fs
@@ -0,0 +1,16 @@
+namespace Xsamf.V1.Operations.Monitoring.Dms
+
+open System
+open Xsamf.V1.Common
+
+[<AutoOpen>]
+module Common =
+    
+    let getNextCheckIn (previousCheckIn: DateTime) (cronExpression: string) =
+        ()
+    
+    
+    
+    
+    ()
+
