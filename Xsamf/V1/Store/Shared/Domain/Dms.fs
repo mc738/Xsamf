697667afb0a46b9caf264ddf2d7a402ceff4dc24
max <me@mclifford.dev>
2024-09-23 09:42:27 +0100

    Work on Dms

--- Xsamf/V1/Store/Shared/Domain/Dms.fs
+++ Xsamf/V1/Store/Shared/Domain/Dms.fs
@@ -1,5 +1,8 @@
 namespace Xsamf.V1.Store.Shared.Domain
 
+open Xsamf.V1.Domain.Monitoring
+open Xsamf.V1.Store.Shared.Domain.Common
+
 module Dms =
 
     open System
@@ -14,7 +17,48 @@ module Dms =
           GracePeriod: TimeSpan
           Tags: string list }
 
-    type DmsHeartBeat =
-        { TenantReference: string
-          Reference: string
-          Timestamp: TimestampType }
+    type NewDmsCheckIn =
+        { WatcherReference: string
+          Reference: string //NewReferenceType
+          Timestamp: DateTime //TimestampType
+          Tags: string list
+          Metadata: Map<string, string> }
+        
+    
+        
+    type DmsWatcherDetails =
+        {
+            TenantReference: string
+            EntityReference: string
+            Reference: string
+            Name: string
+            PreviousCheckInTime: DateTime
+            NextCheckInTime: DateTime
+            ScheduleCron: string
+            GracePeriod: TimeSpan
+            Auth: MonitoringAuthType
+            AllowAnonymous: bool
+            Tags: string list
+            /// <summary>
+            /// A collection of addition metadata that will be added to an activity if the action is triggered.
+            /// These will be used for any future processing from any rule hits from this watcher.
+            /// </summary>
+            AdditionMetadata: Map<string, string>
+            /// <summary>
+            /// A list of addition tags that will be added to an activity if the action is triggered.
+            /// These will be used for any future processing from any rule hits from this watcher.
+            /// </summary>
+            AdditionTags: string list
+        }
+    
+    type DmsWatcherAction =
+        {
+            Reference: string
+            Name: string
+            
+        }
+        
+    //type DmsWatcherAction =
+    //    {
+            
+    //    }
