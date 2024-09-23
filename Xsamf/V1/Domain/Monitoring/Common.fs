989ca137f4613db7909f7dedaa1a3245d06a1223
max <me@mclifford.dev>
2024-09-23 22:22:36 +0100

    Work on Common.fs

--- Xsamf/V1/Domain/Monitoring/Common.fs
+++ Xsamf/V1/Domain/Monitoring/Common.fs
@@ -1,6 +1,8 @@
 namespace Xsamf.V1.Domain.Monitoring
 
 open System.Text.Json
+open FsToolbox.Core
+open FsToolbox.Extensions.Strings
 
 [<AutoOpen>]
 module Common =
@@ -14,6 +16,18 @@ module Common =
     type MonitoringAuthType =
         | None
         | Token of string
+        
+        
+    [<RequireQualifiedAccess>]
+    type HashAlgorithm =
+        | SHA256
+        | None
+        
+        member ha.HashString(str: string) =
+            match ha with
+            | SHA256 -> str.GetSHA256Hash()
+            | None -> str
+            
         
         //static member FromJson(element: JsonElement) =
         //    match element.tryGet
