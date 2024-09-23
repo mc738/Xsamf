077acfb6d93694845ef8b454559d39ff961ec332
max <me@mclifford.dev>
2024-09-23 09:41:29 +0100

    Work on Activities.fs

--- Xsamf/V1/Domain/Monitoring/Activities.fs
+++ Xsamf/V1/Domain/Monitoring/Activities.fs
@@ -1,15 +1,47 @@
 namespace Xsamf.V1.Domain.Monitoring
 
-open System
-
 module Activities =
 
+    open System
+    open System.Text.Json
+    open FsToolbox.Core
+    open Xsamf.V1.Common.Utils
+
+    [<RequireQualifiedAccess>]
     type ActivityCategory =
-        | Information = 0
-        | Success = 1
-        | Warning = 2
-        | Error = 3
-        | Critical = 4
+        | Information
+        | Success
+        | Warning
+        | Error
+        | Failure
+        | Critical
+        | Bespoke of string
+
+        static member Deserialize(str: string) =
+            match str.ToLower() with
+            | "info"
+            | "information" -> ActivityCategory.Information
+            | "success"
+            | "ok" -> ActivityCategory.Success
+            | "warn"
+            | "warning" -> ActivityCategory.Warning
+            | "err"
+            | "error" -> ActivityCategory.Error
+            | "fail"
+            | "failure" -> ActivityCategory.Failure
+            | "crit"
+            | "critical" -> ActivityCategory.Critical
+            | v -> ActivityCategory.Bespoke v
+
+        member ac.Serialize() =
+            match ac with
+            | Information -> "information"
+            | Success -> "success"
+            | Warning -> "warning"
+            | Error -> "error"
+            | Failure -> "failure"
+            | Critical -> "critical"
+            | Bespoke s -> s
 
     type Activity =
         { Reference: string
@@ -25,7 +57,7 @@ module Activities =
         | IsCategory of Category: ActivityCategory
         | HasTag of Tag: string
         | HasTags of Tags: string list
-        | HasMetadata of Key: string
+        | HasMetadataKey of Key: string
         | MetadataValueEquals of Key: string * Value: string
         | Bespoke of HandlerName: string
         | Not of Rule: ActivityRule
@@ -34,12 +66,95 @@ module Activities =
         | All of Rules: ActivityRule list
         | Any of Rules: ActivityRule list
 
+        static member FromJson(element: JsonElement) =
+            match Json.tryGetStringProperty "type" element with
+            | Some t ->
+                match t with
+                | "is-category" ->
+                    match Json.tryGetStringProperty "category" element with
+                    | Some c -> ActivityCategory.Deserialize c |> ActivityRule.IsCategory |> Ok
+                    | None -> Error "Missing `category` property"
+
+                | "has-tag" ->
+                    match Json.tryGetStringProperty "tag" element with
+                    | Some t -> ActivityRule.HasTag t |> Ok
+                    | None -> Error "Missing `tag` property"
+                | "has-tags" ->
+                    Json.tryGetProperty "tags" element
+                    |> Option.map Ok
+                    |> Option.defaultValue (Error "Missing `tags` property")
+                    |> Result.bind (
+                        Json.tryGetStringArray
+                        >> Option.map (ActivityRule.HasTags >> Ok)
+                        >> Option.defaultValue (Error "Could not create tags")
+                    )
+                | "has-metadata-key" ->
+                    match Json.tryGetStringProperty "key" element with
+                    | Some key -> ActivityRule.HasMetadataKey key |> Ok
+                    | None -> Error "Missing `key` property"
+                | "metadata-value-equals" ->
+                    match Json.tryGetStringProperty "key" element, Json.tryGetStringProperty "value" element with
+                    | Some key, Some value -> ActivityRule.MetadataValueEquals(key, value) |> Ok
+                    | None, _ -> Error "Missing `key` property"
+                    | _, None -> Error "Missing `value` property"
+                | "bespoke" ->
+                    match Json.tryGetStringProperty "handlerName" element with
+                    | Some hn -> ActivityRule.Bespoke hn |> Ok
+                    | None -> Error "Missing `handlerName` property"
+                | "not" ->
+                    Json.tryGetProperty "rule" element
+                    |> Option.map ActivityRule.FromJson
+                    |> Option.defaultValue (Error "Missing `rule` property")
+                    |> Result.map ActivityRule.Not
+                | "and" ->
+                    match
+                        Json.tryGetProperty "ruleA" element
+                        |> Option.map ActivityRule.FromJson
+                        |> Option.defaultValue (Error "Missing `ruleA` property"),
+                        Json.tryGetProperty "ruleB" element
+                        |> Option.map ActivityRule.FromJson
+                        |> Option.defaultValue (Error "Missing `ruleB` property")
+                    with
+                    | Ok ruleA, Ok ruleB -> ActivityRule.And(ruleA, ruleB) |> Ok
+                    | Error e, _ -> Error e
+                    | _, Error e -> Error e
+                | "or" ->
+                    match
+                        Json.tryGetProperty "ruleA" element
+                        |> Option.map ActivityRule.FromJson
+                        |> Option.defaultValue (Error "Missing `ruleA` property"),
+                        Json.tryGetProperty "ruleB" element
+                        |> Option.map ActivityRule.FromJson
+                        |> Option.defaultValue (Error "Missing `ruleB` property")
+                    with
+                    | Ok ruleA, Ok ruleB -> ActivityRule.Or(ruleA, ruleB) |> Ok
+                    | Error e, _ -> Error e
+                    | _, Error e -> Error e
+                | "all" ->
+                    match
+                        Json.tryGetArrayProperty "rules" element
+                        |> Option.map (List.map ActivityRule.FromJson >> aggregateStringErrors "Unable to create rules")
+                        |> Option.defaultValue (Error "Missing `rules` property")
+                    with
+                    | Ok rules -> ActivityRule.All rules |> Ok
+                    | Error e -> Error e
+                | "any" ->
+                    match
+                        Json.tryGetArrayProperty "rules" element
+                        |> Option.map (List.map ActivityRule.FromJson >> aggregateStringErrors "Unable to create rules")
+                        |> Option.defaultValue (Error "Missing `rules` property")
+                    with
+                    | Ok rules -> ActivityRule.Any rules |> Ok
+                    | Error e -> Error e
+                | _ -> Error $"Unknown rule type: `{t}`"
+            | None -> Error "Missing `type` property"
+
         member ar.Test(activity: Activity, bespokeHandlers: Map<string, Activity -> bool>) =
             match ar with
             | IsCategory category -> activity.Category = category
             | HasTag tag -> activity.Tags |> List.contains tag
             | HasTags tags -> tags |> List.forall (fun t -> activity.Tags |> List.contains t)
-            | HasMetadata key -> activity.Metadata |> Map.containsKey key
+            | HasMetadataKey key -> activity.Metadata |> Map.containsKey key
             | MetadataValueEquals(key, value) ->
                 activity.Metadata.TryFind key
                 |> Option.map (fun mdv -> mdv.Equals(value, StringComparison.OrdinalIgnoreCase))
@@ -55,6 +170,53 @@ module Activities =
             | Any rules -> rules |> List.exists (fun r -> r.Test(activity, bespokeHandlers))
 
         member ar.Test(activity: Activity) = ar.Test(activity, Map.empty)
+
+        member ar.WriteToJsonValue(writer: Utf8JsonWriter) =
+            Json.writeObject
+                (fun w ->
+                    match ar with
+                    | IsCategory category ->
+                        w.WriteString("type", "is-category")
+                        w.WriteString("category", category.Serialize())
+                    | HasTag tag ->
+                        w.WriteString("type", "has-tag")
+                        w.WriteString("tag", tag)
+                    | HasTags tags ->
+                        w.WriteString("type", "has-tags")
+                        Json.writeArray (fun aw -> tags |> List.iter aw.WriteStringValue) "tags" writer
+                    | HasMetadataKey key ->
+                        w.WriteString("type", "has-metadata-key")
+                        w.WriteString("key", key)
+                    | MetadataValueEquals(key, value) ->
+                        w.WriteString("type", "metadata-value-equals")
+                        w.WriteString("key", key)
+                        w.WriteString("value", value)
+                    | Bespoke handlerName ->
+                        w.WriteString("type", "bespoke")
+                        w.WriteString("handlerName", handlerName)
+                    | Not rule ->
+                        w.WriteString("type", "not")
+                        w.WritePropertyName("rule")
+                        rule.WriteToJsonValue(w)
+                    | And(ruleA, ruleB) ->
+                        w.WriteString("type", "and")
+                        w.WritePropertyName("ruleA")
+                        ruleA.WriteToJsonValue(w)
+                        w.WritePropertyName("ruleB")
+                        ruleB.WriteToJsonValue(w)
+                    | Or(ruleA, ruleB) ->
+                        w.WriteString("type", "or")
+                        w.WritePropertyName("ruleA")
+                        ruleA.WriteToJsonValue(w)
+                        w.WritePropertyName("ruleB")
+                        ruleB.WriteToJsonValue(w)
+                    | All rules ->
+                        w.WriteString("type", "all")
+                        Json.writeArray (fun aw -> rules |> List.iter (fun r -> r.WriteToJsonValue aw)) "rules" w
+                    | Any rules ->
+                        w.WriteString("type", "any")
+                        Json.writeArray (fun aw -> rules |> List.iter (fun r -> r.WriteToJsonValue aw)) "rules" w)
+                writer
 
     type ActivityAction =
         {
