49598b71a431e77a893b307d6f04049525d45416
max <me@mclifford.dev>
2024-09-25 20:41:59 +0100

    Work on Dms.fs

--- Xsamf/V1/Services/Monitoring/Dms.fs
+++ Xsamf/V1/Services/Monitoring/Dms.fs
@@ -1,6 +1,8 @@
 namespace Xsamf.V1.Services.Monitoring
 
+open FsToolbox.Core.Results
 open Xsamf.V1.Domain.Monitoring.Dms
+open Xsamf.V1.Operations.Monitoring.Dms
 open Xsamf.V1.Store.Shared
 
 
@@ -9,6 +11,13 @@ type DmsService(store: IXsamfStore) =
     
     member _.AnonymousCheckIn(checkIn: AnonymousDmsCheckIn) =
         // First get the watcher
+        ReadOperations.getWatcher store checkIn.WatcherReference
+        |> FetchResult.toActionResult
+        |> ActionResult.bind (fun watcher ->
+            
+            
+            
+            ())
         
         
         // First try and add the check in
