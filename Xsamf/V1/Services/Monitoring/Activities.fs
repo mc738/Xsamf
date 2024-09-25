13780e59d7738a621decf23955820a733af100af
max <me@mclifford.dev>
2024-09-25 20:41:21 +0100

    Work on Activities.fs

--- Xsamf/V1/Services/Monitoring/Activities.fs
+++ Xsamf/V1/Services/Monitoring/Activities.fs
@@ -5,14 +5,8 @@ open FsToolbox.Core.Results
 open Xsamf.V1.Domain.Monitoring.Activities
 open Xsamf.V1.Store.Shared
 open Xsamf.V1.Operations.Monitoring
+open Xsamf.V1.Common.Utils
 
-module ActionResult =
-
-    let ifSuccessful<'T, 'U> (fn: unit -> ActionResult<'U>) (result: ActionResult<'T>) =
-        match result with
-        | ActionResult.Failure failureResult -> result
-        | ActionResult.Success foo -> fn () |> ActionResult.bind (fun _ -> result)
-        
 type ActivitiesService(store: IXsamfStore) =
 
     member _.HandleActivity(activity: Activity, bespokeHandlers: Map<string, Activity -> bool>) =
@@ -20,21 +14,25 @@ type ActivitiesService(store: IXsamfStore) =
         |> ActionResult.fromFetchResult
         |> ActionResult.ifSuccessful (fun _ -> Activities.CreateOperations.addActivity store activity)
         |> ActionResult.bind (fun watcher ->
-            let filteredResults = watcher.HandleActivity(activity, bespokeHandlers, true)
-            
+            let filteredResults = watcher.HandleActivity(activity, bespokeHandlers)
+
             match filteredResults.IsEmpty with
             | true -> ()
             | false ->
-                let tags = activity.Tags @ watcher.AdditionTags
-                let metadata = activity.Metadata |> Map.merge watcher.AdditionMetadata
-                
+                //let tags = activity.Tags @ watcher.AdditionTags
+                //let metadata = activity.Metadata |> Map.merge watcher.AdditionMetadata
+
                 filteredResults
                 |> List.map (fun result ->
                     // Handle the results
-                    let resultTags = tags @ result.AdditionTags
-                    let resultMetadata = activity.Metadata |> Map.merge watcher.AdditionMetadata
+                    //let resultTags = tags @ result.AdditionTags
+                    //let resultMetadata = activity.Metadata |> Map.merge watcher.AdditionMetadata
+                    
+                    
 
                     ())
+
+                ()
 
 
             // Mark the activity as handled.
