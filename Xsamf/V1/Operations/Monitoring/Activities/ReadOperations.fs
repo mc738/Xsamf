35291fc6c1265cd4e65881ce35c04a33a05ca93f
max <me@mclifford.dev>
2024-09-26 22:49:15 +0100

    Work on ReadOperations.fs

---
+++ Xsamf/V1/Operations/Monitoring/Activities/ReadOperations.fs
@@ -0,0 +1,11 @@
+namespace Xsamf.V1.Operations.Monitoring.Activities
+
+module ReadOperations =
+
+    open FsToolbox.Core.Results
+    open Xsamf.V1.Domain.Monitoring.Activities
+    open Xsamf.V1.Store.Shared
+        
+    let getWatcherForActivity (reader: IXsamfStoreReader) (activity: Activity) : FetchResult<ActivityWatcher> =
+        failwith "TODO"
+
