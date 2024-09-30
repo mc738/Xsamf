01059535e84b2344feca6bdd4bd97a56b4cf7a5d
max <me@mclifford.dev>
2024-09-30 23:20:36 +0100

    Work on ReadOperations.fs

---
+++ Xsamf/V1/Operations/Monitoring/Dms/ReadOperations.fs
@@ -0,0 +1,16 @@
+namespace Xsamf.V1.Operations.Monitoring.Dms
+
+open FsToolbox.Core.Results
+open Xsamf.V1.Domain.Monitoring.Dms
+open Xsamf.V1.Store.Shared
+
+module ReadOperations =
+    
+    let getWatcher (reader: IXsamfStoreReader) (reference: string) : FetchResult<DmsWatcher> =
+        
+        failwith "TODO"
+        
+    
+    
+    ()
+
