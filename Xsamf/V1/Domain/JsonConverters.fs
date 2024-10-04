8d7aa584c9f4b3b19be5df76eb8f24e1ba0600bf
max <me@mclifford.dev>
2024-10-04 18:46:33 +0100

    Added ActivityHasher JsonConverters.fs

--- Xsamf/V1/Domain/JsonConverters.fs
+++ Xsamf/V1/Domain/JsonConverters.fs
@@ -7,7 +7,7 @@ module JsonConverters =
     open System.Text.Json
     open System.Text.Json.Serialization
     open Xsamf.V1.Domain.Monitoring.Dms
-
+   
     type DmsRuleConverter() =
 
         inherit JsonConverter<DmsRule>()
@@ -36,6 +36,22 @@ module JsonConverters =
 
             match ActivityRule.FromJson doc.RootElement with
             | Ok rule -> rule
+            | Error errorValue -> raise (JsonException(errorValue))
+
+        override this.Write(writer, value, options) = value.WriteToJsonValue writer
+        
+    type ActivityHasherConverter() =
+
+        inherit JsonConverter<ActivityHasher>()
+
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match ActivityHasher.FromJson doc.RootElement with
+            | Ok hasher -> hasher
             | Error errorValue -> raise (JsonException(errorValue))
 
         override this.Write(writer, value, options) = value.WriteToJsonValue writer
