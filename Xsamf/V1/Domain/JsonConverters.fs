f351a88775f48e2b9d415fb1f3da639ec6f2cdaf
max <me@mclifford.dev>
2024-10-05 19:41:14 +0100

    Added json converter

--- Xsamf/V1/Domain/JsonConverters.fs
+++ Xsamf/V1/Domain/JsonConverters.fs
@@ -1,5 +1,6 @@
 namespace Xsamf.V1.Domain
 
+open Xsamf.V1.Domain.Monitoring
 open Xsamf.V1.Domain.Monitoring.Activities
 
 module JsonConverters =
@@ -8,6 +9,21 @@ module JsonConverters =
     open System.Text.Json.Serialization
     open Xsamf.V1.Domain.Monitoring.Dms
  
+    type ActionOutcomeConverter() =
+        inherit JsonConverter<ActionOutcome>()
+        
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match ActionOutcome.FromJson doc.RootElement with
+            | Ok outcome -> outcome
+            | Error errorValue -> raise (JsonException(errorValue))
+
+        override this.Write(writer, value, options) = value.WriteToJsonValue writer
+    
     type DmsRuleConverter() =
 
         inherit JsonConverter<DmsRule>()
