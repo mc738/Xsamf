4a4e775cf61954423576bc8ee79b067535959c81
max <me@mclifford.dev>
2024-10-06 13:27:08 +0100

    Finished up get activity watcher

--- Xsamf/V1/Store/SQLite/SQLiteXsamfStore.fs
+++ Xsamf/V1/Store/SQLite/SQLiteXsamfStore.fs
@@ -24,36 +24,4 @@ type SQLiteXsamfStore(ctx: SqliteContext) =
         member this.DmsCheckIn(var0) = failwith "todo"
 
         member this.GetActivityWatcher(reference, version) =
-            let (conditions, parameters) =
-                match version with
-                | ItemVersion.Latest -> [ "WHERE watcher_id = @0 ORDER BY version DESC LIMIT 1" ], [ box reference ]
-                | ItemVersion.Specific version ->
-                    [ "WHERE watcher_id = @0 AND version = @1" ], [ box reference; box version ]
-
-            Operations.selectActivityWatcherRecord ctx [ "WHERE id = @0" ] [ id ]
-            |> Option.bind (fun aw ->
-                Operations.selectActivityWatcherVersionRecord ctx conditions parameters
-                |> Option.map (fun awv -> aw, awv))
-            |> Option.map (fun (aw, awv) ->
-                ({ Reference = aw.Id
-                   Name = awv.Name
-                   TenantReference = ""
-                   EntityReference = aw.EntityId
-                   Actions = failwith "todo"
-                   AdditionMetadata =
-                     Operations.selectActivityWatcherVersionMetadataItemRecords
-                         ctx
-                         [ "WHERE version_id = @0" ]
-                         [ awv.Id ]
-                     |> List.map (fun amd -> amd.ItemKey, amd.ItemValue)
-                     |> Map.ofList
-                   AdditionTags =
-                     Operations.selectActivityWatcherVersionTagsRecords ctx [ "WHERE version_id = @0" ] [ awv.Id ]
-                     |> List.map (fun at -> at.Tag) }
-                : ActivityWatcher)
-                |> FetchResult.Success)
-            |> Option.defaultWith (fun _ ->
-                { Message = ""
-                  DisplayMessage = ""
-                  Exception = None }
-                |> FetchResult.Failure)
+            Activities.getWatcherVersion ctx reference version
