809e0f264f475c00f12f05cd876c9b154d7a75d9
max <me@mclifford.dev>
2024-10-07 23:09:03 +0100

    Added AddMonitoringEvent

--- Xsamf/V1/Store/Shared/IXsamfStoreWriter.fs
+++ Xsamf/V1/Store/Shared/IXsamfStoreWriter.fs
@@ -1,14 +1,15 @@
 namespace Xsamf.V1.Store.Shared
 
 open FsToolbox.Core.Results
+open Xsamf.V1.Domain.Monitoring.Events
 open Xsamf.V1.Store.Shared.Domain.HeartBeats
 
 type IXsamfStoreWriter =
-    
-    
+
+
     abstract member WriteTest: unit -> unit
-    
-    
+
+
     abstract member HeartBeatCheckIn: NewHeartBeatCheckIn -> ActionResult<unit>
-    
-    
+
+    abstract member AddMonitoringEvents: TenantReference: string * Events: MonitoringEvent list -> ActionResult<unit>
